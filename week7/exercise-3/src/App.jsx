747503f0bf5e18dff22fc8dafe91634c699a6141
lyyeakkhai <yeakhai.ly@student.cadt.edu.kh>
2026-03-08 13:20:16 +0700

    Complete exercise 3

--- week7/exercise-3/src/App.jsx
+++ week7/exercise-3/src/App.jsx
@@ -1,9 +1,41 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
-  /* You will need to use many state to keep the inut values and other needs */
+  /* You will need to use many state to keep the input values and other needs */
+  const [a, setA] = useState("");
+  const [b, setB] = useState("");
+  const [result, setResult] = useState("");
+  const [error, setError] = useState(false);
 
   /* You will need some function to handle the key pressed and button events */
+  function onA(e) {
+    setA(e.target.value);
+
+  }
+
+  function onB(e) {
+    setB(e.target.value);
+  }
+
+  function validateInput() {
+    if (isNaN(a) || isNaN(b)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function onCompute(e) {
+    e.preventDefault();
+    if(validateInput()){
+      setError(true);
+      setResult("A and B should be numbers");
+      return;
+    }
+    setError(false);
+    setResult(Number(a) + Number(b));
+  }
 
   return (
     <main>
@@ -17,9 +49,9 @@ function App() {
 
       <label>A + B =</label>
 
-      {/* When Compute buton is clicked, this input display the sum of the 2 numbers, or the error message in RED */}
-      <input disabled />
-      <button>Compute</button>
+      {/* When Compute buton is clicked, thiscls input display the sum of the 2 numbers, or the error message in RED */}
+      <input disabled value={result} className={error ? "error" : ""} />
+      <button onClick={onCompute}>Compute</button>
     </main>
   );
 }
